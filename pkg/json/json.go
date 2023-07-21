da5e7d1cb3eb5e3d0d2a745d3dc1b053f8994fed
warnerb47 <taphagaye2001.mg@gmail.com>
2023-07-21 15:47:35 +0000

    refactor

---
+++ pkg/json/json.go
@@ -0,0 +1,15 @@
+package json
+
+import (
+	"fmt"
+	"os"
+)
+
+func loadJson() {
+	jsonFile, err := os.Open("users.json")
+	defer jsonFile.Close()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Successfully Opened users.json")
+}
