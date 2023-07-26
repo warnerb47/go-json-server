e474a46e824e6d10fc0108ecc41998d0ed0b9602
warnerb47 <taphagaye2001.mg@gmail.com>
2023-07-26 16:42:41 +0000

    feat: integrate addEntity

--- pkg/router/router.go
+++ pkg/router/router.go
@@ -31,6 +31,18 @@ func getEntityById(id string, entities []any) (any, error) {
 	return nil, errors.New("Entity not found")
 }
 
+func addEntity(c *gin.Context, data []any) {
+	var entity any
+
+	if err := c.BindJSON((&entity)); err != nil {
+		return
+	}
+
+	data = append(data, entity)
+	c.IndentedJSON(http.StatusCreated, entity)
+
+}
+
 func getEntity(c *gin.Context, data []any) {
 	id := c.Param("id")
 	entity, err := getEntityById(id, data)
@@ -47,11 +59,10 @@ func getEntities(c *gin.Context, data []any) {
 
 func Configure(data map[string]any) *gin.Engine {
 	router := gin.Default()
+	var enpoints []string
 
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"status": "ok"})
-	})
 	for key := range data {
+		enpoints = append(enpoints, key)
 		entities, ok := formatEntities(key, data)
 		if ok != nil {
 			fmt.Printf("Invalid JSON structure: %v\n", key)
@@ -62,6 +73,13 @@ func Configure(data map[string]any) *gin.Engine {
 		router.GET("/"+key+"/:id", func(c *gin.Context) {
 			getEntity(c, entities)
 		})
+		router.POST("/"+key, func(c *gin.Context) {
+			addEntity(c, entities)
+		})
 	}
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"enpoints": enpoints})
+	})
 	return router
 }
