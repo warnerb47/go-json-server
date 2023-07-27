60715d628685914463896b9e7f73ffd02b49eda6
warnerb47 <taphagaye2001.mg@gmail.com>
2023-07-27 13:11:45 +0000

    feat: add WriteJson

--- pkg/fileLoader/fileLoader.go
+++ pkg/fileLoader/fileLoader.go
@@ -7,7 +7,7 @@ import (
 	"os"
 )
 
-func LoadJson() map[string]interface{} {
+func LoadJson() map[string]any {
 	jsonFile, err := os.Open("./data.json")
 	defer jsonFile.Close()
 	if err != nil {
@@ -17,7 +17,22 @@ func LoadJson() map[string]interface{} {
 	if err != nil {
 		fmt.Println(err)
 	}
-	var result map[string]interface{}
+	var result map[string]any
 	json.Unmarshal(byteValue, &result)
 	return result
+}
+
+func WriteJson(key string, data any) error {
+	jsonData := LoadJson()
+	jsonData[key] = data
+
+	byte, err := json.Marshal(jsonData)
+	if err != nil {
+		return err
+	}
+	err = ioutil.WriteFile("data.json", byte, 0644)
+	if err != nil {
+		return err
+	}
+	return nil
 }
